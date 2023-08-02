bc6e892f75d13ec7dd7d60ed9774e5a2863f33a2
jouNative <j.ndevba@gmail.com>
2023-08-02 02:49:40 -0300

    add routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,16 @@
-import { Routes, Route } from "react-router-dom"
-import { Dashboard } from "./page/Dashboard"
-
+import { Routes, Route } from 'react-router-dom'
+import { Dashboard } from './page/Dashboard'
+import { UFPage } from './page/UF'
+import { MunicipioPage } from './page/Municipio'
+import { BairroPage } from './page/Bairro'
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
+      <Route path="/uf" element={<UFPage />} />
+      <Route path="/municipio" element={<MunicipioPage />} />
+      <Route path="/bairro" element={<BairroPage />} />
     </Routes>
   )
 }
